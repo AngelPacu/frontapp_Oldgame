606b7ac00b8fc43948a8901dab7ed49ac98313a4
AngelPacu <angelpacu@outlook.com>
2024-05-03 12:34:13 +0200

    Privaticacion de rutas y refresh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
-import React from 'react';
+import React, {useContext} from 'react';
 
 // Imports Propios
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage';
 import AuthPage from './AuthPage';
-import { AuthProvider } from './AuthContext';
+import { AuthProvider, AuthContext } from './AuthContext';
+
+// Ruta pública que redirige a /home_user si el usuario está autenticado
+const PublicRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  if (user) {
+      return <Navigate to="/home_user" />;
+  }
+  return children;
+};
+
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
         <Route path="/home_user" element={<AuthPage />} />
         {/* Asegúrate de que el resto de tus rutas estén aquí, cada una con su propio componente */}
       </Routes>
--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -12,11 +12,35 @@ export const AuthContext = createContext({
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
+
+    // Verificamos si el usuario está autenticado
+    useEffect(() => {
+      const verifyUser = async () => {
+          try {
+              const response = await fetch('http://localhost:4000/api/verify', {
+                  method: 'GET',
+                  credentials: 'include'
+              });
+              // Si la respuesta es correcta, asignamos el usuario
+              if (response.ok) {
+                  const userData = await response.json();
+                  setUser(userData);
+              } else {
+                  setUser(null);
+              }
+          } catch (error) {
+              console.error('Error al verificar el usuario:', error);
+          }
+      };
+      verifyUser();
+  }, [navigate]);
+
     // Función de login y asignadmos el usuario
     const login = (user) => {
     setUser(user);
   };
 
+  // Configuración de la petición
   const fetchConfig = {
     method: 'POST',
     credentials: 'include',  // Esto asegura que se envíen cookies
@@ -32,6 +56,8 @@ export const AuthProvider = ({ children }) => {
       })
       .catch(error => console.error('Error al cerrar sesión:', error)); // Maneja errores
   };
+  
+  // Devolvemos el contexto con el usuario y las funciones de login y logout
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
